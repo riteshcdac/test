9421c0c172b21dc9ef3ad5275ad0a1403a80dba4
riteshcdac <riteshcdac@hotmail.com>
2021-01-02 08:31:40 -0800

    Changes from dummy first time

--- hungry.py
+++ hungry.py
@@ -1,4 +1,4 @@
-print ("This comes from Thirsty brnach")
+print ("This comes from Dummy brnach, which will be deleted soon")
 isHungry=input("Are you hungry?")
 if isHungry=="yes":
     print("Packup! Lets go to eat")
